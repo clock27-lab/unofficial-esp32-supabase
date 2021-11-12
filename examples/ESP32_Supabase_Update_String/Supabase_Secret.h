086feab756a02f821c301d457f43aee0d70aad57
wind city <fahmi.sangengon@gmail.com>
2021-11-13 05:26:17 +0700

    Update Supabase_Secret.h

--- examples/ESP32_Supabase_Update_String/Supabase_Secret.h
+++ examples/ESP32_Supabase_Update_String/Supabase_Secret.h
@@ -1,10 +1,9 @@
 // Supabase Cresidential
 
-
-#define supabaseUrl  "https://ltdrerokdqmenbbsvlol.supabase.co/rest/v1"
-#define tableRealtime  "esp32_realtime"
-#define supabaseApiKey  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2NzQ0MDgxLCJleHAiOjE5NTIzMjAwODF9.N2iAX-Vum2io8xU2XzwXbyC6Mobg2-5Sg3BQm0occs0"
-#define supabaseBearer "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2NzQ0MDgxLCJleHAiOjE5NTIzMjAwODF9.N2iAX-Vum2io8xU2XzwXbyC6Mobg2-5Sg3BQm0occs0"
+#define supabaseUrl  ""
+#define tableRealtime  ""
+#define supabaseApiKey  ""
+#define supabaseBearer "Bearer "
 
 
 // Supabase Cresidential
